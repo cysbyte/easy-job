469bc35228d9359173b75dd849efdc4fca0dee9a
cysbyte <chenyunshu2@gmail.com>
2024-09-06 12:39:31 +0800

    feat: implement home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
-
+import Header from "./components/home/Header";
+import Hero from "./components/home/Hero";
+import HowItWorks from "./components/home/HowItWorks";
+import Questions from "./components/home/Questions";
+import Footer from "./components/shared/Footer";
 export default function Home() {
   return (
-   <header className="">
-main
-   </header>
+    <main>
+    <Header/>
+    <Hero/>
+    <HowItWorks/>
+    <Questions/>
+    <Footer/>
+  </main>
   );
 }
